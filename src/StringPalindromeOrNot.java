ae5bbfc492bc7d8df49033028e1fb984a7e7d2d2
himanshu <himanshugupta@Himanshus-Mac-mini.local>
2026-04-16 20:50:54 +0530

    commit init

--- src/StringPalindromeOrNot.java
+++
@@ -1,9 +0,0 @@
-void main() {
-
-
-    String name = "Himanshu";
-
-    boolean isPalindrome = IntStream.range(0, name.length()/2)
-            .allMatch(i -> name.charAt(i) == name.charAt(name.length()-i-1));
-    System.out.println(isPalindrome);
-}
